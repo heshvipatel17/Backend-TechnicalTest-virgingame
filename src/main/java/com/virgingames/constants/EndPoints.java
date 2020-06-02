42065867ff63bc07c10707b2816580504fcafdec
Heshvipatel17 <heshvipatel17@gmail.com>
2020-06-03 00:51:19 +0100

    Technical test Beckend

---
+++ src/main/java/com/virgingames/constants/EndPoints.java
@@ -0,0 +1,9 @@
+package com.virgingames.constants;
+
+/* By Jitendra Patel */
+
+public class EndPoints {
+
+    public static final String END_POINT = "/GetBingoLobbyFeed.do";
+
+}
---
+++ src/main/java/com/virgingames/constants/Path.java
@@ -0,0 +1,9 @@
+package com.virgingames.constants;
+
+/* By Jitendra Patel */
+
+public class Path {
+
+    public static final String PATH = "/bingo";
+
+}
